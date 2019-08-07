fc17d8d8b82193089498a4ba2d3460140ce89043
Bruno Petrolini <brunopetrolini@hotmail.com>
2019-08-07 11:26:45 -0300

    Add controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const DevController = require("./controllers/DevController");
+const LikeController = require("./controllers/LikeController");
+const DislikeController = require("./controllers/DislikeController");
 
 const routes = express.Router();
 
@@ -6,8 +9,10 @@ routes.get("/", (req, res) => {
   return res.json({ message: `Olá ${req.query.name}` });
 });
 
-routes.post("/devs", (req, res) => {
-  return res.json(req.body);
-});
+routes.get("/devs", DevController.index);
+routes.post("/devs", DevController.store);
+
+routes.post("/devs/:devId/likes", LikeController.store);
+routes.post("/devs/:devId/dislikes", DislikeController.store);
 
 module.exports = routes;
